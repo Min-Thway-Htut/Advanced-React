135c0b2cba0f4d0eb6a812239ab7c5a269d0da1e
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-11-10 19:29:46 +0100

    Another update

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -3,23 +3,35 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-  const [name, setName] = useState("");
+
+  const [score, setScore] = useState("10");
+  const [comment, setComment] = useState("");
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    setName("");
-    console.log("Form submitted");
-  }
+    if (Number(score) <= 5 && comment.length <= 10){
+      alert("Please provide a comment explaining why the experience was poor.")
+    }
+
+    console.log("Form submitted!");
+    setComment("");
+    setScore("10");
+  } 
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
       <fieldset>
+        <h2>Feedback form</h2>
         <div className="Field">
-          <label htmlFor="name">Name:</label>
-          <input id="name" type="text" placeholder="Name" name="name" value={name} onChange={e => setName(e.target.value)}/>
+          <label>Score: {score}</label>
+          <input type="range" min="0" max="10" />
         </div>
-        <button disabled={!name} type="submit">Submit</button>
+        <div className="Field">
+          <label>Comment</label>
+          <textarea value={comment} onChange={e => setComment(e.target.value)}></textarea>
+        </div>
+        <button  type="submit">Submit</button>
       </fieldset>
       </form>
     </div>
