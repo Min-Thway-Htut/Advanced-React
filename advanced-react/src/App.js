8f840fa0f4f103bc82198cdab628979626199f20
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-12-09 22:38:00 +0100

    UseContext

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,41 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react";
+import React, {createContext, useState} from 'react';
+import ComponentOne from './ComponentOne';
+import ComponentTwo from './ComponentTwo';
 
-function App() {
+export const MyContext = createContext();
 
-  const [score, setScore] = useState("10");
-  const [comment, setComment] = useState("");
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (Number(score) <= 5 && comment.length <= 10){
-      alert("Please provide a comment explaining why the experience was poor.")
-    }
-
-    console.log("Form submitted!");
-    setComment("");
-    setScore("10");
-  } 
+function App () {
+  const [data, setData] = useState('Hello from ComponentOne!');
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-      <fieldset>
-        <h2>Feedback form</h2>
-        <div className="Field">
-          <label>Score: {score}</label>
-          <input type="range" min="0" max="10" />
-        </div>
-        <div className="Field">
-          <label>Comment</label>
-          <textarea value={comment} onChange={e => setComment(e.target.value)}></textarea>
-        </div>
-        <button  type="submit">Submit</button>
-      </fieldset>
-      </form>
-    </div>
-  );
+    <MyContext.Provider value={{data, setData}}>
+      <div>
+        <h1>This is the App Component</h1>
+        <ComponentOne />
+        <ComponentTwo />
+      </div>
+    </MyContext.Provider>
+  )
 }
 
 export default App;
