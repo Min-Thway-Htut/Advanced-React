671629dfcc1f084d977c9763a2193e1ca93d22ed
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-12-14 17:26:00 +0100

    Another update

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,41 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 import {useState} from "react";
 
-function App() {
+export default function App(){
+  const [greeting, setGreeting] = useState({greet: "Hello, World"});
+  console.log(greeting, setGreeting);
 
-  const [score, setScore] = useState("10");
-  const [comment, setComment] = useState("");
+  function updateGreeting() {
+    const newGreeting = {...greeting};
+    newGreeting.greet = "Hello, World-Wide Web";
+    setGreeting(newGreeting);
+
+  }
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (Number(score) <= 5 && comment.length <= 10){
-      alert("Please provide a comment explaining why the experience was poor.")
-    }
-
-    console.log("Form submitted!");
-    setComment("");
-    setScore("10");
-  } 
-
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-      <fieldset>
-        <h2>Feedback form</h2>
-        <div className="Field">
-          <label>Score: {score}</label>
-          <input type="range" min="0" max="10" />
-        </div>
-        <div className="Field">
-          <label>Comment</label>
-          <textarea value={comment} onChange={e => setComment(e.target.value)}></textarea>
-        </div>
-        <button  type="submit">Submit</button>
-      </fieldset>
-      </form>
+    <div>
+      <h1>{greeting.greet}</h1>
+      <button onClick={updateGreeting}>Update greeting</button>
     </div>
-  );
+  )
 }
-
-export default App;
