376b08cb941138ad7cf9a1546791b8e14fd4c851
Min-Thway-Htut <minthwayhtut568@gmail.com>
2025-01-02 20:38:34 +0100

    Adding Mouse Position

--- advanced-react/src/App.js
+++ advanced-react/src/App.js
@@ -1,39 +1,70 @@
-import * as React from "react";
+import {useState, useEffect} from 'react';
 
-const Row = ({children, spacing}) => {
-   
-    const childStyle = {
-        marginLeft: `${spacing}px`,
+
+const withMousePosition  = (WrappedComponent) => {
+    return (props) => {
+
+        const [mousePosition, setMousePosition] = useState({
+            x: 0,
+            y: 0,
+        })
+
+        useEffect(() => {
+            const handleMousePositionChange = (e) => {
+                setMousePosition({
+                    x: e.clientX,
+                    y: e.clientX,
+                });
+            };
+
+            window.addEventListener('mousemove', handleMousePositionChange);
+
+            return () => {
+                window.removeEventListener('mousemove', handleMousePositionChange);
+            }
+        }, [])
+        return <WrappedComponent {...props} mousePosition={mousePosition} />
     }
-   
+}
+
+const PanelMouseLogger = ({mousePosition}) => {
+    if(!mousePosition){
+        return null;
+    }
     return (
         <div>
-            {React.Children.map(children, (child, index) => {
-                return React.cloneElement(child,
-                    {
-                        style: {
-                            ...child.props.style,
-                            ...(index > 0 ? childStyle : {}),
-                        }
-                    }
-                );
-            })}
+            <p>Mouse Position</p>
+            <div>
+                <span>x: {mousePosition.x}</span>
+                <span>y: {mousePosition.y}</span>
+            </div>
         </div>
     );
 };
 
-function LiveOrders() {
+const PointMouseLogger = ({mousePosition}) => {
+    if(!mousePosition){
+        return null;
+    }
+    return (
+        <p>
+            ({mousePosition.x}, {mousePosition.y})
+        </p>
+    );
+};
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
+
+
+function App(){
     return (
         <div>
-     <Row spacing={32}>
-        <p>Pizza Margarita</p>
-        <p>2</p>
-        <p>30 dollars</p>
-        <p>18: 30</p>
-        <p>John</p> 
-    </Row>       
+            <header>Little Lemon Restaurane</header>
+            <PanelMouseTracker />
+            <PointMouseTracker />
         </div>
     )
 }
 
-export default LiveOrders;
+export default App;
